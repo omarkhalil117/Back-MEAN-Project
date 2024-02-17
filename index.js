63ccbb927765882a3e0ef2b296b79d0ca6ebf4f8
omarkhalil117 <omarkhalil117@gmail.com>
2024-02-17 14:42:47 +0200

    first step

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = 3000;
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('Connected to db'))
+  .catch((err) => console.log(err.message));
+
+app.use((err, req, res) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.get('/', (req, res) => res.send('Hello World!'));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
