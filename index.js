1df4bd0b093b3d25fbb215584d593e5456cec351
Mohamed-Ayman <eng.developer.mohamed.ayman@gmail.com>
2024-02-17 21:09:20 +0200

    file structure

--- index.js
+++ index.js
@@ -3,16 +3,21 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
-const port = 3000;
 
 mongoose.connect(process.env.MONGODB_URI_LOCAL)
   .then(() => console.log('Connected to db'))
   .catch((err) => console.log(err));
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.all('*', (req, res, next) => {
+  next('Error');
+});
 
 app.use((err, req, res) => {
   console.error(err);
   res.status(500).send('Something broke!');
 });
 
-app.get('/', (req, res) => res.send('Hello World!'));
+const port = 3000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
