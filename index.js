acdeb3d2f4e919f4c7b64f24e716ed944efa44df
EngAlaaKamal <alaa.kamal.ghonem@gmail.com>
2024-02-18 15:23:24 +0200

    add category endpoint

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const categoryRoutes =require('./routes/categoryRoutes')
 
 const app = express();
 const port = 3000;
 
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => console.log("Connected to db"))
-  .catch((err) => console.log(err.message));
+mongoose.connect(process.env.MONGODB_URI_LOCAL)
+  .then(() => console.log('Connected to db'))
+  .catch((err) => console.log(err));
 
+app.use(express.json());
+app.use(categoryRoutes);
 app.use((err, req, res) => {
-  console.error(err.stack);
-  res.status(500).send("Something broke!");
+  console.error(err);
+  res.status(500).send('Something broke!');
 });
+ 
 
-app.get("*", (req, res) => res.status(404).json({ message: "Not found" }));
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
