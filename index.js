ea5d8b5b92f6e470ec789ac8c5ab88790a37f278
EngAlaaKamal <alaa.kamal.ghonem@gmail.com>
2024-02-18 15:03:59 +0200

    add global get request

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = 3000;
 
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('Connected to db'))
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to db"))
   .catch((err) => console.log(err.message));
 
 app.use((err, req, res) => {
   console.error(err.stack);
-  res.status(500).send('Something broke!');
+  res.status(500).send("Something broke!");
 });
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get("*", (req, res) => res.status(404).json({ message: "Not found" }));
+app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
