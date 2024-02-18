1383953869bc9bafa581ab5560fe28144975789f
“sara-aref” <sara.aref.hassan@gmail.com>
2024-02-18 21:31:12 +0200

    Book Model

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-require('dotenv').config();
+require('dotenv').config({ path: './config.env' });
 const express = require('express');
 const mongoose = require('mongoose');
+
+const bookRouter = require('./routes/bookRouter');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 const port = 3000;
 
-mongoose.connect(process.env.MONGODB_URI)
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGODB_URI_LOCAL)
   .then(() => console.log('Connected to db'))
-  .catch((err) => console.log(err.message));
+  .catch((err) => console.log(err));
+
+app.use('/books', bookRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`can't find ${req.originalUrl} on this server`, 404));
+});
+
+app.use(globalErrorHandler);
 
 app.use((err, req, res) => {
-  console.error(err.stack);
+  console.error(err);
   res.status(500).send('Something broke!');
 });
 
