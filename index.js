b921a6904fa956fce87be622d29032210ab7a4a3
Mohamed-Ayman <eng.developer.mohamed.ayman@gmail.com>
2024-02-25 01:37:00 +0200

    last update

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static('uploads'));
-
+dotenv.config('.env');
 mongoose.connect(process.env.MONGODB_URI_LOCAL)
   .then(() => console.log('Connected to db'))
   .catch((err) => console.log(err));
